894e53381e133f8ae3d46a48cc2eec297bac96ba
Logan McNaughton <logan@bacoosta.com>
2017-09-04 21:56:53 -0600

    again

--- build.cmd
+++ build.cmd
@@ -1,13 +1,13 @@
-msbuild mupen64plus-core/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release|%myarch%
+msbuild mupen64plus-core/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release /p:Platform=%myarch%
 
-msbuild mupen64plus-rsp-cxd4/projects/VisualStudio2013/mupen64plus-rsp-cxd4.vcxproj /p:Configuration=Release|%myarch%
+msbuild mupen64plus-rsp-cxd4/projects/VisualStudio2013/mupen64plus-rsp-cxd4.vcxproj /p:Configuration=Release /p:Platform=%myarch%
 
-msbuild mupen64plus-input-sdl/projects/VisualStudio2013/mupen64plus-input-sdl.vcxproj /p:Configuration=Release|%myarch%
+msbuild mupen64plus-input-sdl/projects/VisualStudio2013/mupen64plus-input-sdl.vcxproj /p:Configuration=Release /p:Platform=%myarch%
 
-msbuild mupen64plus-audio-sdl/projects/VisualStudio2013/mupen64plus-audio-sdl.vcxproj /p:Configuration=Release|%myarch%
+msbuild mupen64plus-audio-sdl/projects/VisualStudio2013/mupen64plus-audio-sdl.vcxproj /p:Configuration=Release /p:Platform=%myarch%
 
-msbuild mupen64plus-ui-console/projects/VisualStudio2013/mupen64plus-ui-console.vcxproj /p:Configuration=Release|%myarch%
+msbuild mupen64plus-ui-console/projects/VisualStudio2013/mupen64plus-ui-console.vcxproj /p:Configuration=Release /p:Platform=%myarch%
 
 REM do gui here
 
-msbuild GLideN64/projects/msvc12/GLideN64.sln /p:Configuration=Release_mupenplus|%myarch%
+msbuild GLideN64/projects/msvc12/GLideN64.sln /p:Configuration=Release_mupenplus /p:Platform=%myarch%
