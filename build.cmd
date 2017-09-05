33ee8172ee3663c67d1ea0dc2c05c3f50e67d21e
Logan McNaughton <logan@bacoosta.com>
2017-09-04 21:46:32 -0600

    more1

--- build.cmd
+++ build.cmd
@@ -1,1 +1,13 @@
 msbuild mupen64plus-core/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release
+
+msbuild mupen64plus-rsp-cxd4/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release
+
+msbuild mupen64plus-input-sdl/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release
+
+msbuild mupen64plus-audio-sdl/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release
+
+msbuild mupen64plus-ui-console/projects/VisualStudio2013/mupen64plus-core.vcxproj /p:Configuration=Release
+
+REM do gui here
+
+msbuild GLideN64/projects/msvc12/GLideN64.sln /p:Configuration=Release_mupenplus
--- build.sh
+++ build.sh
@@ -9,9 +9,7 @@ if [[ $1 == "rpi3" ]]; then
 fi
 
 UNAME=$(uname -s)
-if [[ $UNAME == *"MINGW"* ]]; then
-  suffix=".dll"
-elif [[ $UNAME == "Darwin" ]]; then
+if [[ $UNAME == "Darwin" ]]; then
   suffix=".dylib"
 else
   if [[ $HOST_CPU != "i686" ]]; then
@@ -61,15 +59,7 @@ cp $base_dir/mupen64plus-ui-console/projects/unix/mupen64plus* $install_dir
 if [[ $1 != "rpi3" ]]; then
   mkdir -p $base_dir/mupen64plus-gui/build
   cd $base_dir/mupen64plus-gui/build
-  if [[ $UNAME == *"MINGW"* ]]; then
-    if [[ $UNAME == *"MINGW64"* ]]; then
-      /mingw64/qt5-static/bin/qmake ../mupen64plus-gui.pro
-    else
-      /mingw32/qt5-static/bin/qmake ../mupen64plus-gui.pro
-    fi
-    make -j4 release
-    cp $base_dir/mupen64plus-gui/build/release/mupen64plus-gui.exe $install_dir
-  elif [[ $UNAME == "Darwin" ]]; then
+  if [[ $UNAME == "Darwin" ]]; then
     /usr/local/Cellar/qt5/*/bin/qmake ../mupen64plus-gui.pro
     make -j4
     cp -Rp $base_dir/mupen64plus-gui/build/mupen64plus-gui.app $install_dir
@@ -85,51 +75,16 @@ cd $base_dir/GLideN64/src
 cd $base_dir/GLideN64/projects/cmake
 if [[ $1 == "rpi3" ]]; then
   cmake -DNOHQ=On -DUSE_SYSTEM_LIBS=On -DCRC_OPT=On -DNEON_OPT=On -DVEC4_OPT=On -DMUPENPLUSAPI=On ../../src/
-elif [[ $UNAME == *"MINGW"* ]]; then
-  cmake -G "MSYS Makefiles" -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
 else
   cmake -DUSE_SYSTEM_LIBS=On -DVEC4_OPT=On -DCRC_OPT=On -DMUPENPLUSAPI=On ../../src/
 fi
 make -j4
 
-if [[ $UNAME == *"MINGW"* ]]; then
-  cp mupen64plus-video-GLideN64$suffix $install_dir
-else
-  cp plugin/release/mupen64plus-video-GLideN64$suffix $install_dir
-fi
+cp plugin/release/mupen64plus-video-GLideN64$suffix $install_dir
 cp $base_dir/GLideN64/ini/GLideN64.custom.ini $install_dir
 cd $base_dir
 
-if [[ $UNAME == *"MINGW"* ]]; then
-  if [[ $UNAME == *"MINGW64"* ]]; then
-    my_os=win64
-    my_path=mingw64
-    cp /$my_path/bin/libgcc_s_seh-1.dll $install_dir
-  else
-    my_os=win32
-    my_path=mingw32
-    cp /$my_path/bin/libgcc_s_dw2-1.dll $install_dir
-  fi
-  cp /$my_path/bin/libwinpthread-1.dll $install_dir
-  cp /$my_path/bin/SDL2.dll $install_dir
-  cp /$my_path/bin/libpcre16-0.dll $install_dir
-  cp /$my_path/bin/libpng16-16.dll $install_dir
-  cp /$my_path/bin/libglib-2.0-0.dll $install_dir
-  cp /$my_path/bin/libstdc++-6.dll $install_dir
-  cp /$my_path/bin/zlib1.dll $install_dir
-  cp /$my_path/bin/libintl-8.dll $install_dir
-  cp /$my_path/bin/libpcre-1.dll $install_dir
-  cp /$my_path/bin/libiconv-2.dll $install_dir
-  cp /$my_path/bin/libharfbuzz-0.dll $install_dir
-  cp /$my_path/bin/libgraphite2.dll $install_dir
-  cp /$my_path/bin/libfreetype-6.dll $install_dir
-  cp /$my_path/bin/libbz2-1.dll $install_dir
-  cp /$my_path/bin/libminizip-1.dll $install_dir
-  cp /$my_path/bin/libsamplerate-0.dll $install_dir
-  cp /$my_path/bin/libspeexdsp-1.dll $install_dir
-  cp /$my_path/bin/libjasper-4.dll $install_dir
-  cp /$my_path/bin/libjpeg-8.dll $install_dir
-elif [[ $UNAME == "Darwin" ]]; then
+if [[ $UNAME == "Darwin" ]]; then
   my_os=macos
 
   find mupen64plus -type f -depth 1 \
